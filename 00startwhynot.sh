2326edb67d3193736409c2750b63e13db408464c
valetzx <45054400+valetzx@users.noreply.github.com>
2022-10-17 11:41:52 +0000

    fix commit

--- 00startwhynot.sh
+++ 00startwhynot.sh
@@ -1,6 +1,7 @@
 sudo zerotier-one -d
 sleep 2
-sudo rm -rf /tmp/onedrive
+if [ ! -d "/tmp/onedrive" ];then
+#sudo rm -rf /tmp/onedrive
 sudo rm -rf /tmp/rclone
 sudo mkdir /tmp/ariadown
 sudo mkdir /tmp/rclone
@@ -8,19 +9,26 @@ sudo mkdir /tmp/onedrive
 sudo rclone mount e5: /tmp/onedrive --allow-non-empty --daemon --vfs-cache-mode writes --allow-other --cache-dir /tmp/rclone && df -hl
 sudo ls /tmp/onedrive/
 #sudo service netdata restart
+sleep 5
+sudo docker restart e5renew
+sleep 2
 sudo docker restart xubuntu
 sleep 2
 sudo docker restart ubuntu
 sleep 2
 sudo docker restart ariang
+sudo docker restart alist
 sleep 2
-sudo docker restart aria2-Pro
+sudo docker restart aria2-pro
 sleep 2
+sudo docker restart socks5
 sudo v2hidealist/v2ray/sbin/v2ray -config v2hidealist/v2ray/etc/config.json >/dev/null 2>&1 &
 #sudo bt restart
 #sudo vncserver :1 
 #sudo ./ngx restart
-sudo nethogs
+#sudo nethogs
+fi
+sudo htop
 #PORT:
 #v2ray:10000
 #netdata:19999
@@ -32,4 +40,4 @@ sudo nethogs
 #ngxcloud:8090
 #e5renew:1066
 #ariang:6880
-#aria:6800
+echo done 
--- tmpwrite.py
+++ tmpwrite.py
@@ -1,5 +1,5 @@
-2022-06-19+11:33:27+Codespaces-tmpwrite.py-spacest[Codespaces]-VisualStudioCode
-2022-06-19+19:33:27+ea5b70bfa
-2022-06-19+19:33:27+ea5b70bfa
-2022-06-19+19:33:27+ea5b70bfa
-50b84323e+272629791 
+2022-10-13+15:03:31+tmpwrite.py-/[Codespaces]-VisualStudioCodeMozillaFirefox
+2022-10-13+23:03:31+2c3624a31
+
+
+        ff6ebebe5+83886888388611        
